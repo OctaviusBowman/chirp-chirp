0bae6ef48b751da28b33d2939e61a5bf27a7b9d1
Octavius Bowman <octaviusbowman@gmail.com>
2023-04-09 14:15:03 -0400

    Loading state of posting and toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 
 dayjs.extend(relativeTime)
 
@@ -11,6 +11,7 @@ import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -19,10 +20,18 @@ const CreatePostWizard = () => {
 
   const ctx = api.useContext();
 
-  const {mutate, isLoading: isPosting} = api.posts.create.useMutation({
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
       setInput(""),
-      void ctx.posts.getAll.invalidate()
+        void ctx.posts.getAll.invalidate()
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0])
+      } else {
+        toast.error("Failed to post! Please try again later.")
+      }
     }
   })
 
@@ -33,16 +42,29 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex gap-3 w-full">
-      <Image src={user.profileImageUrl} alt="Profile Image" width={56} height={56}  className="w-14 h-14 rounded-full" />
-      <input 
-      placeholder="Type some emoji's" 
-      className="bg-transparent grow outline-none" 
-      type="text"
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-      disabled={isPosting}
+      <Image src={user.profileImageUrl} alt="Profile Image" width={56} height={56} className="w-14 h-14 rounded-full" />
+      <input
+        placeholder="Type some emoji's"
+        className="bg-transparent grow outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input})
+            }
+          }
+        }}
       />
-      <button onClick={() => mutate({content: input})}>Post</button>
+      {input !== "" && !isPosting && (<button onClick={() => mutate({ content: input })} >Post</button>)}
+      {isPosting && (
+        <div className="flex justify-center items-center">
+          <LoadingSpinner />
+        </div>
+      )}
     </div>
   )
 
@@ -53,7 +75,7 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div key={post.id} className="p-4 gap-3 border-b border-slate-400 flex">
-      <Image src={author.profilePicture} className="w-14 h-14 rounded-full" width={56} height={56} alt={`@${author.username}'s profile picture`}/>
+      <Image src={author.profilePicture} className="w-14 h-14 rounded-full" width={56} height={56} alt={`@${author.username}'s profile picture`} />
       <div className="flex flex-col">
         <div className="flex text-slate-300 gap-1"><span>{`@${author.username}`}</span><span className="font-thin">{` • ${dayjs(post.createdAt).fromNow()}`}</span></div>
         <span className="text-2xl">{post.content}</span>
